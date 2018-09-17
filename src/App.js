c70ce6b24e288c99626ea7e9725b424e9099d775
Montell Francis <montelldfrancis@gmail.com>
2018-09-17 17:55:42 -0400

    changes Component to component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "../src/assets/css/App.css";
-import Header from './components/Header';
-import Index from './components/Index';
-import Footer from './components/Footer';
-import Products from './components/Products';
-import Error from './components/Error';
-import Contact from './components/Contact'
-
-
+import Header from "./components/Header";
+import Index from "./components/Index";
+import Footer from "./components/Footer";
+import Products from "../src/components/Products";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div>
-        <Header />
-        <Switch>
-            <Route path="/" Component={Index} exact />
-            <Route path="/products" Component={Products} />
-            <Route path="/contact" Component={Contact} />
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" component={Index} exact />
+            <Route path="/products" component={Products} exact />
+            <Route path="/contact" component={Contact} exact />
             <Route component={Error} />
-        </Switch>  
-        <Footer />
-      </div>  
-      </BrowserRouter>  
-      
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
