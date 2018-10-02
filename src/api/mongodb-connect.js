ad8ea065616f2833f0a33d6ea9ca1593f4a38b26
Montell Francis <montelldfrancis@gmail.com>
2018-10-02 09:02:29 -0400

    working with api

--- src/api/mongodb-connect.js
+++ src/api/mongodb-connect.js
@@ -9,20 +9,19 @@ MongoClient.connect('mongodb://localhost:27017/Ecommerce', (err, client) => {
     const db = client.db('Ecommerce')
 
     db.collection('Products').insertOne({
-                title: "OFF-WHITE x Air Jordan 1 Retro High OG 'UNC'",
+        title: "OFF-WHITE Red Checked",
 
-                productType:"shoes",
-                productBrand:"off-white",
-                productImage:"/img/products/shoe1.jpeg",
-                price: 750,
-                description : "Inspired by Michael Jordan’s alma mater, the Off-White x Air Jordan 1 Retro High OG ‘UNC’ carries a classic two-tone composition, filtered through Virgil Abloh’s unique design prism.",
+        productType:"shirts",
+        productBrand:"off-white",
+        productImage:"/img/products/shirt3.jpg",
+        price: 655,
+        description: "Long sleeves shirt featuring classic tartan pattern in tones of red, blue and black. Spread collar. Buttons closure at front and 'CHECK SHIRT' text print at front. Oversize fitting.",
     }, (err, result) => {
         if (err) {
             return console.log('Unable to insert product', err);
         }
 
         console.log(JSON.stringify(result.ops, undefined, 2));
-            // console.log(result.ops);
     });
 
     client.close();
