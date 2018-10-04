d1f0638c5f37d2b68548d7441ce4abe0fb0ac49e
Montell Francis <montelldfrancis@gmail.com>
2018-10-03 23:53:17 -0400

    adds express

--- src/api/server/server.js
+++ src/api/server/server.js
@@ -1,2 +1,4 @@
-var {mongoose} = require('./db/mongoose');
+var express = require('express');
+import { mongoose } from './db/mongoose';
+import { Product } from './models/product';
 //when time to add admin page return to sec 7 lec 71
